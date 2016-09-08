552f46440c075dc312a5c1d08cc5197ec08b1fb1
junneyang <597092663@qq.com>
2016-09-08 13:13:59 +0800

    DEMO AFT

--- src/com/ares/hadoop/mr/flowsum/FlowSumMapper.java
+++ src/com/ares/hadoop/mr/flowsum/FlowSumMapper.java
@@ -8,27 +8,19 @@ import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.util.StringUtils;
 import org.apache.log4j.Logger;
 
-import com.ares.hadoop.mr.exception.LineException;
 import com.ares.hadoop.mr.wordcount.MRTest;
 
 //Long, String, String, Long --> LongWritable, Text, Text, LongWritable
 public class FlowSumMapper extends Mapper<LongWritable, Text, Text, FlowBean> {
 	private static final Logger LOGGER = Logger.getLogger(MRTest.class);
 	
-	enum Counter {
-		LINESKIP
-	}
-	
 	private String line;
 	private int length;
-	private final static char separator = '\t';
 	
 	private String phoneNum;
 	private long upFlow;
 	private long downFlow;
-	//private long sumFlow;
-	
-	private Text text = new Text();
+	private long sumFlow;
 	private FlowBean flowBean = new FlowBean();
 	
 	@Override
@@ -37,41 +29,19 @@ public class FlowSumMapper extends Mapper<LongWritable, Text, Text, FlowBean> {
 			throws IOException, InterruptedException {
 		// TODO Auto-generated method stub
 		//super.map(key, value, context);
-		try {
-			line = value.toString();
-			String[] fields = StringUtils.split(line, separator);
-			length = fields.length;
-			if (length != 11) {
-				throw new LineException(key.get() + ", " + line + " LENGTH INVALID, IGNORE...");
-			}
-			
-			phoneNum = fields[1];
-			upFlow = Long.parseLong(fields[length-3]);
-			downFlow = Long.parseLong(fields[length-2]);
-			//sumFlow = upFlow + downFlow;
-			
-			flowBean.setPhoneNum(phoneNum);
-			flowBean.setUpFlow(upFlow);
-			flowBean.setDownFlow(downFlow);
-			//flowBean.setSumFlow(sumFlow);
-			
-			text.set(phoneNum);
-			context.write(text, flowBean);
-		} catch (LineException e) {
-			// TODO: handle exception
-			LOGGER.error(e);
-			context.getCounter(Counter.LINESKIP).increment(1);
-			return;
-		} catch (NumberFormatException e) {
-			// TODO: handle exception
-			LOGGER.error(key.get() + ", " + line + " FLOW DATA INVALID, IGNORE...");
-			context.getCounter(Counter.LINESKIP).increment(1);
-			return;
-		} catch (Exception e) {
-			// TODO: handle exception
-			LOGGER.error(e);
-			context.getCounter(Counter.LINESKIP).increment(1);
-			return;
+		line = value.toString();
+		String[] fields = StringUtils.split(line);
+		length = fields.length;
+		if (length < 9) {
+			LOGGER.error(key.get() + ", " + line + " INVALID, IGNORE...");
 		}
+		phoneNum = fields[1];
+		upFlow = Long.parseLong(fields[length-3]);
+		downFlow = Long.parseLong(fields[length-2]);
+		sumFlow = upFlow + downFlow;
+		flowBean.setPhoneNum(phoneNum);
+		flowBean.setUpFlow(upFlow);
+		flowBean.setDownFlow(downFlow);
+		flowBean.setSumFlow(sumFlow);
 	}
 }
